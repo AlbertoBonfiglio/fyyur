c902297fbcac46028f2de799274691abe60874d5
albertobonfiglio <darthbert>
2023-06-22 22:09:47 +1000

    (dev) Search * implemented search in venue and artist

--- controllers/artist.py
+++ controllers/artist.py
@@ -2,7 +2,7 @@ from models import db, Artist, Venue, Show
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_wtf import Form
 from forms import ArtistForm
-
+from sqlalchemy.orm import load_only
 #  Artists
 #  ----------------------------------------------------------------
 def artists():
@@ -13,22 +13,20 @@ def artists():
   return render_template('pages/artists.html', artists=data)
 
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
+  # TODO [X]: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
+  qry = request.form.get('search_term', '')
+  data = Artist.query.filter(Artist.name.ilike(f'%{qry}%')).all()
   response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+    "count": len(data),
+    "data": data
   }
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 def show_artist(artist_id):
   # shows the artist page with the given artist_id
-  # TODO [ ]: replace with real artist data from the artist table, using artist_id
+  # TODO [X]: replace with real artist data from the artist table, using artist_id
   data = Artist.query.get(artist_id)
   return render_template('pages/show_artist.html', artist=data)
 
